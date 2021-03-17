8ad1837f154207d6014c75f444cdc6b5a8815602
RahilBaharia <79479601+RahilBaharia@users.noreply.github.com>
2021-03-16 23:26:00 -0500

    laptop froze got part of the code going and design

---
+++ forms/calculator/calculator.js
@@ -0,0 +1,8 @@
+num1 = enterNumber1.value
+num2 = enterNumber2.value
+
+
+Button5.onclick=function(){
+  let ouput1 = (num1 * num2)
+}
+
---
+++ forms/newControls/newControls.js
@@ -0,0 +1,26 @@
+
+Radiobutton1.onchange=function(){
+  let RDButton = Radiobutton1.value
+}
+
+
+
+Select1.onchange=function(){
+  let selectVal = Select1.value
+}
+
+
+
+Dropdown1.onclick=function(){
+  let dropDwn = Dropdown1.value
+}
+
+
+
+Button6.onclick=function(){
+  let RDButton = Radiobutton1.value
+  let selectVal = Select1.value
+  let dropDwn = Dropdown1.value
+  let name1 = inputName.value
+    labelOutput.value = (`${name} your favorite meal of the day is ${RDButton} and you like to`)
+}
