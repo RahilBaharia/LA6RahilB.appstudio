16971c9d90a24c7ce779a11b8c73a84a1dd83b60
RahilBaharia <79479601+RahilBaharia@users.noreply.github.com>
2021-03-16 22:41:27 -0500

    membership

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,16 @@
-/*
 let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+
+
+Button1.onclick=function(){
+  let name = Input1.value
+  members.push(name)
+  console.log(members)
+  alert(`${name} you have a membership`)
+}
+
+Button2.onclick=function(){
+    let name = Input1.value
+    members.push(name)
+    lableOutput.value = (`${name} you do not have a membership`)
+}
